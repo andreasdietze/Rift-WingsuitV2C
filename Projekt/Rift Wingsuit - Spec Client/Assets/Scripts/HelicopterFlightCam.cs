f3fcc049979d6512125bf8a704a7d7eacc6f196e
Andreas Dietze <stag_o_saurus@hotmail.com>
2016-09-03 19:50:14 +0200

    Server-Client content synchronisation: - New world assets and prefabs - Cloudsystem - New player assets and prefabs - Cleaned project filepaths - Camera prefab

---
+++ Projekt/Rift Wingsuit - Spec Client/Assets/Scripts/HelicopterFlightCam.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelicopterFlightCam : MonoBehaviour {
+
+    //public FlyCamV2 flycam;
+    /*public HelicopterFlight heliflight;
+    private bool hasStarted = false;
+    private bool pushOut = false;
+    private bool isRunning = true;
+    public float startTime = 1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.J) && !hasStarted && isRunning)
+        {
+            hasStarted = true;
+            // StartCoroutine(StartFlightRoutine());
+            heliflight.GoHeliStart();
+            flycam.StartFlight();
+            isRunning = false;
+        }
+	}
+
+    void FixedUpdate()
+    {
+        if (isRunning)
+        {
+            Vector3 flightVector = new Vector3(heliflight.flightSpeed, 0, 0);
+            transform.Translate(flightVector);
+            if (hasStarted)
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * heliflight.startSpeed);
+            }
+        }
+    }
+
+    IEnumerator StartFlightRoutine()
+    {
+        yield return new WaitForSeconds(startTime);
+        heliflight.GoHeliStart();
+        flycam.StartFlight();
+        isRunning = false;
+    }*/
+}
---
+++ Projekt/Rift Wingsuit - Spec Client/Assets/World1/Clouds/CloudsManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudsManager : MonoBehaviour {
+
+	// Different settings for cloud systems.
+	public enum Weather  {Sunny, Cloudy, Stormy};
+	public Weather weather = Weather.Sunny;
+
+	// Handle activity of different cloud systems.
+	private GameObject topLevelClouds 	= null;
+	private GameObject midLevelClouds 	= null;
+	private GameObject mountainClouds 	= null;
+
+	// Settings for different cloud systems.
+	private GameObject clouds 			= null;
+	private GameObject cloudLayer 		= null;
+	private GameObject mountainTop	 	= null;
+
+	void Start () {
+
+		//(Controller)GameObject.Find("RiftCam").GetComponent("KinectController");
+		topLevelClouds = GameObject.Find ("TopLevel");
+		midLevelClouds = GameObject.Find ("MidLevel");
+		mountainClouds = GameObject.Find ("MountainClouds");
+
+		// Default and initial weather is sunny. 
+		switch(weather) {
+			// Only skydome clouds.
+		case Weather.Sunny :
+			topLevelClouds.SetActive (false);
+			midLevelClouds.SetActive (false);
+			mountainClouds.SetActive (false);
+			break;
+			// Top and midLevel clouds.
+		case Weather.Cloudy :
+			topLevelClouds.SetActive (true);
+			midLevelClouds.SetActive (true);
+			mountainClouds.SetActive (false);
+			break;
+			// All cloud systems.
+		case Weather.Stormy :
+			topLevelClouds.SetActive (true);
+			midLevelClouds.SetActive (true);
+			mountainClouds.SetActive (true);
+			break;
+		default: weather = Weather.Sunny;
+			break;
+		}
+	}
+
+	void Update () {
+	
+	}
+}
