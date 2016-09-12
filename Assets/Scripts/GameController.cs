fc03ee162fd30da612c956efae5908e9412338b3
elsen <manuel.schmitt@informatik.hs-fulda.de>
2016-09-12 17:46:49 +0200

    Set IPInput

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public static GameController instance = null;
+    public string ip;
+    public int currentScene;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+        currentScene = 0;
+    }
+
+    void Update()
+    {
+        //controls
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.JoystickButton1))
+        {
+            switch(currentScene)
+            {
+                case 0:
+                    Application.Quit();
+                    break;
+                case 1:
+                    SceneManager.LoadScene(0);
+                    currentScene = 0;
+                    break;
+            }
+        }
+
+
+
+        //scenes
+        switch(currentScene)
+        {
+            case 0:
+                break;
+            case 1:
+                break;
+        }
+    }
+}
