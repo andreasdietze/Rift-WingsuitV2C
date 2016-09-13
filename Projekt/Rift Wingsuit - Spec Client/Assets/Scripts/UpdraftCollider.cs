ce7eaad5299990086c87a1c9ada959f86b8faf3c
andreasdietze <stag_o_saurus@hotmail.com>
2016-09-13 13:38:40 +0200

    Update client updraft / booster

---
+++ Projekt/Rift Wingsuit - Spec Client/Assets/Scripts/UpdraftCollider.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpdraftCollider : MonoBehaviour {
+
+    public float UpdraftStrength = 40;
+
+	// Use this for initialization
+	void OnTriggerEnter(Collider other)
+    {
+        Rigidbody rBody = other.GetComponentInChildren<Rigidbody>();
+        if(rBody == null)
+        {
+            Debug.LogWarning("Rigidbody in collider is null!");
+        } else
+        {
+            Vector3 vel = new Vector3(rBody.velocity.x, UpdraftStrength, rBody.velocity.z);
+            rBody.velocity = vel;
+        }
+    }
+}
